b4af71c5caad2392dcbc0d540db7eddeda1d7487
Ganfong <ganfong90@gmail.com>
2021-12-16 09:13:26 +0000

    config

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,12 @@
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
- 	
+import vercel from '@sveltejs/adapter-vercel';
 
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
+const config = {
+    kit: {
+   	 // hydrate the <div id="svelte"> element in src/app.html
+   	 target: '#svelte',
+   	 adapter: vercel()
+    }
 };
 
 export default config;
